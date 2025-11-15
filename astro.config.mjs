595c0c93504e4b1a29d3b2d19c0b874760ce7a05
Fränze Kantreiter, geb. Lingott <fraenze.lingott@home24.de>
2025-11-15 20:11:52 +0000

    feat: add sitemap integration and update configuration
    - Integrated `@astrojs/sitemap` into the Astro configuration for sitemap generation.
    - Updated `package.json` and `package-lock.json` to include `@astrojs/sitemap` as a dependency.
    - Added `robots.txt` file to specify the sitemap URL and disallow all user agents.
    - Enhanced `Testimonial.astro` component for better layout and styling.
    - Fixed image path in `404.astro` for correct asset loading.
    - Updated `index.astro` with SEO-related comments and optimized image handling notes.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   // trailingSlash: 'always',
-  integrations: [tailwind()]
+  site: 'https://kita-grossweil.netlify.app/',
+  integrations: [
+    tailwind(),
+    sitemap()
+  ]
 });
